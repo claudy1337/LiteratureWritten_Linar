11753c07d95df5c186ad5885b9bfd1ff6bd91ae3
claudy1337 <45602451+claudy1337@users.noreply.github.com>
2022-06-30 01:03:37 +0300

    Добавьте файлы проекта.

---
+++ LiteratureWritten/Pages/AddEditionsPage.xaml.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LiteratureWritten.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для AddEditionsPage.xaml
+    /// </summary>
+    public partial class AddEditionsPage : Page
+    {
+        public static Classes.User User;
+        public AddEditionsPage(Classes.User user)
+        {
+            User = user;
+            InitializeComponent();
+            Model.SubscribedEditions SearchEditions = Model.BDConnection.bd.SubscribedEditions.FirstOrDefault(e=>e.UserID == User.Id);
+            ListEdition.ItemsSource = Model.BDConnection.bd.Editions.Where(u=> u.Status == true && u.ID != SearchEditions.Editions.ID).ToList();
+            CBDeliveryMethod.ItemsSource = Model.BDConnection.bd.DeliveryMethod.ToList();
+            CBTerm.ItemsSource = Model.BDConnection.bd.SubscriptionTerm.ToList();
+        }
+
+        private void ListEdition_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var select = ListEdition.SelectedItem as Model.Editions;
+            txtName.Text = select.Name;
+            txtType.Text = select.EditionType1.Name;
+        }
+
+        private void BtnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (string.IsNullOrWhiteSpace(CBDeliveryMethod.Text) && string.IsNullOrWhiteSpace(CBTerm.Text))
+                {
+                    return;
+                }
+                else
+                {
+                    var selectEdition = ListEdition.SelectedItem as Model.Editions;
+                    var selectDelivery = CBDeliveryMethod.SelectedItem as Model.DeliveryMethod;
+                    var selectTerm = CBTerm.SelectedItem as Model.SubscriptionTerm;
+                    
+                    Model.SubscribedEditions subscribed = new Model.SubscribedEditions
+                    {
+                        Date = DateTime.Now,
+                        Edition = selectEdition.ID,
+                        SubscriptionTerm = selectTerm.ID,
+                        DeliveryMethod = selectDelivery.ID,
+                        UserID = User.Id,
+                    };
+                    Model.BDConnection.bd.SubscribedEditions.Add(subscribed);
+                    Model.BDConnection.bd.SaveChanges();
+                    MessageBox.Show("add");
+                }
+                
+            }
+            catch(Exception)
+            {
+                return;
+            }
+            
+        }
+
+        private void TextBlock_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            NavigationService.Navigate(new Pages.SubscribedPage(User));
+        }
+    }
+}
---
+++ LiteratureWritten/Pages/SubscribedPage.xaml.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LiteratureWritten.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для SubscribedPage.xaml
+    /// </summary>
+    public partial class SubscribedPage : Page
+    {
+        public static Classes.User User;
+        public SubscribedPage(Classes.User user)
+        {
+            User = user;
+            InitializeComponent();
+            CBSearch.ItemsSource = Model.BDConnection.bd.EditionType.ToList();
+            if (User.Role == 2)
+            {
+                ListEdition.ItemsSource = Model.BDConnection.bd.SubscribedEditions.Where(u => u.UserID == User.Id && u.Editions.Status == true).ToList();
+                txtUserEdit.Visibility = Visibility.Hidden;
+            }
+            else
+            {
+                ListEdition.ItemsSource = Model.BDConnection.bd.SubscribedEditions.Where(u=>u.Editions.Status == true).ToList();
+            }
+
+            
+        }
+
+        private void ListEdition_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var selectEdition = ListEdition.SelectedItem as Model.SubscribedEditions;
+            txtCostEdit.Text = "price:" + selectEdition.Editions.Price;
+            txtNameEdit.Text = "name:" + selectEdition.Editions.Name;
+            txtUserEdit.Text = "user:" + selectEdition.Users.Login;
+            txtTermEdit.Text = "term:" + selectEdition.SubscriptionTerm1.Term;
+            txtPereodicityEdit.Text = "period:" + selectEdition.SubscriptionTerm1.Periodicity1.Name;
+            txtDateEdit.Text = "date:" + selectEdition.Date.ToString();
+            txtDelivery.Text = "delivery:" + selectEdition.DeliveryMethod1.Name;
+            txtTypeEdit.Text = "type:" + selectEdition.Editions.EditionType1.Name;
+        }
+       
+        public void Refresh()
+        {
+            txtCostEdit.Text = "";
+            txtNameEdit.Text = "";
+            txtUserEdit.Text = "";
+            txtTermEdit.Text = "";
+            txtPereodicityEdit.Text = "";
+            txtDateEdit.Text = "";
+            txtDelivery.Text = "";
+            txtTypeEdit.Text = "";
+        }
+        private void BtnExit_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow mainWindow = new MainWindow();
+            mainWindow.Show();
+        }
+
+        private void BtnAddEdit_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new Pages.AddEditionsPage(User));
+        }
+
+        private void txtSearch_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            Refresh();
+            if (User.Role == 2)
+            {
+                
+                ListEdition.ItemsSource = Model.BDConnection.bd.SubscribedEditions.Where(u => u.UserID == User.Id && u.Editions.Name == txtSearch.Text && u.Editions.Status == true).ToList();
+            }
+            else
+            {
+                ListEdition.ItemsSource = Model.BDConnection.bd.SubscribedEditions.Where(u=> u.Users.Login == txtSearch.Text || u.Editions.Name == txtSearch.Text).ToList();
+            }
+        }
+
+        private void CBSearch_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Refresh();
+            var select = CBSearch.SelectedItem as Model.EditionType;
+            if (User.Role == 2)
+            {
+                ListEdition.ItemsSource = Model.BDConnection.bd.SubscribedEditions.Where(u => u.UserID == User.Id && u.Editions.EditionType1.ID == select.ID && u.Editions.Status == true).ToList();
+                txtUserEdit.Visibility = Visibility.Hidden;
+            }
+            else
+            {
+                
+                ListEdition.ItemsSource = Model.BDConnection.bd.SubscribedEditions.Where(u=>u.Editions.EditionType1.ID == select.ID && u.Editions.Status == true).ToList();
+            }
+        }
+
+        private void BtnEdit_Click(object sender, RoutedEventArgs e)
+        {
+            
+            var selectEdition = ListEdition.SelectedItem as Model.SubscribedEditions;
+            if (selectEdition == null)
+            {
+                MessageBox.Show("select item");
+                return;
+            }
+            else
+            {
+                Classes.Edition edition = new Classes.Edition(selectEdition.ID, User, selectEdition.Editions.Name, selectEdition.Editions.EditionType1.Name);
+                NavigationService.Navigate(new EditEditionPage(edition));
+            }
+            
+        }
+    }
+}
