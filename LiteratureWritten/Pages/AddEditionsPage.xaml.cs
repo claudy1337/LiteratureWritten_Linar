5285ac4a92d95b505f3b128e67eea4c7414b6163
claudy1337 <45602451+claudy1337@users.noreply.github.com>
2022-06-30 02:48:26 +0300

    all fix

--- LiteratureWritten/Pages/AddEditionsPage.xaml.cs
+++ LiteratureWritten/Pages/AddEditionsPage.xaml.cs
@@ -26,7 +26,7 @@ namespace LiteratureWritten.Pages
             User = user;
             InitializeComponent();
             Model.SubscribedEditions SearchEditions = Model.BDConnection.bd.SubscribedEditions.FirstOrDefault(e=>e.UserID == User.Id);
-            ListEdition.ItemsSource = Model.BDConnection.bd.Editions.Where(u=> u.Status == true && u.ID != SearchEditions.Editions.ID).ToList();
+            ListEdition.ItemsSource = Model.BDConnection.bd.Editions.Where(u=> u.Status == true).ToList();
             CBDeliveryMethod.ItemsSource = Model.BDConnection.bd.DeliveryMethod.ToList();
             CBTerm.ItemsSource = Model.BDConnection.bd.SubscriptionTerm.ToList();
         }
@@ -36,6 +36,7 @@ namespace LiteratureWritten.Pages
             var select = ListEdition.SelectedItem as Model.Editions;
             txtName.Text = select.Name;
             txtType.Text = select.EditionType1.Name;
+            txtPrice.Text = select.Price;
         }
 
         private void BtnAdd_Click(object sender, RoutedEventArgs e)
@@ -51,18 +52,31 @@ namespace LiteratureWritten.Pages
                     var selectEdition = ListEdition.SelectedItem as Model.Editions;
                     var selectDelivery = CBDeliveryMethod.SelectedItem as Model.DeliveryMethod;
                     var selectTerm = CBTerm.SelectedItem as Model.SubscriptionTerm;
-                    
-                    Model.SubscribedEditions subscribed = new Model.SubscribedEditions
+                    Model.SubscribedEditions editions = Model.BDConnection.bd.SubscribedEditions.FirstOrDefault(u=>u.UserID == User.Id && u.Editions.ID == selectEdition.ID);
+                    if (editions == null)
                     {
-                        Date = DateTime.Now,
-                        Edition = selectEdition.ID,
-                        SubscriptionTerm = selectTerm.ID,
-                        DeliveryMethod = selectDelivery.ID,
-                        UserID = User.Id,
-                    };
-                    Model.BDConnection.bd.SubscribedEditions.Add(subscribed);
+                        Model.SubscribedEditions subscribed = new Model.SubscribedEditions
+                        {
+                            Date = DateTime.Now,
+                            Edition = selectEdition.ID,
+                            SubscriptionTerm = selectTerm.ID,
+                            DeliveryMethod = selectDelivery.ID,
+                            UserID = User.Id,
+                            Status = true
+                        };
+                        Model.BDConnection.bd.SubscribedEditions.Add(subscribed);
+                        MessageBox.Show("add");
+                    }
+                    else
+                    {
+                        editions.Date = DateTime.Now;
+                        editions.DeliveryMethod = selectDelivery.ID;
+                        editions.SubscriptionTerm = selectTerm.ID;
+                        MessageBox.Show("re-recording");
+                        
+                    }  
                     Model.BDConnection.bd.SaveChanges();
-                    MessageBox.Show("add");
+                    
                 }
                 
             }
